4c4c7e07c926ad6c6ff1d6c300437ce3d0d1fdd4
Jakub Maślanka <jakubadammaslanka@gmail.com>
2023-04-28 15:39:09 +0200

    added remove tasks

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
 {
     const tasks = [];
 
-    const render = () => {
-        let htmlString = ""
-
-        for (const task of tasks)
-            htmlString += `
-            <li class="list__items">
-            <button class="list__button--done">✓</button>
-            ${task.content}
-            <button class="list__button--remove">🗑</button>
-            </li>
-        `;
-
-        document.querySelector(".js-list").innerHTML = htmlString;
-
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const render = () => {
+        let htmlString = ""
+
+        for (const task of tasks)
+            htmlString += `
+            <li class="list__items">
+            <button class="list__button--done js-done">✓</button>
+            ${task.content}
+            <button class="list__button--remove js-remove">🗑</button>
+            </li>
+        `;
+
+        document.querySelector(".js-list").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+            removeTask(index);
+            });
+        });
     };
 
     const onFormSubmit = (event) => {
